423322d0639204818293ffdd2f0b781d07953b32
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-17 17:20:16 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)
+        for candidate in possible_anagrams:
+            if sorted(candidate) == sorted_word:
+                matches.append(candidate)
+        return matches
